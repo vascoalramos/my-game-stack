8cea3c6d58abfdddf43527f5692574f2c55f2723
Vasco Ramos <vascoalramos@outlook.pt>
2019-04-27 16:55:33 +0100

    Added creation if Event table

--- BD.sql
+++ BD.sql
@@ -13,8 +13,18 @@ create table [User](
 );
 
 create table EventType(
-	ID		int				identity(1,1)	not null,	-- auto -increment feature
+	TypeID	int				identity(1,1)	not null,	-- auto -increment feature
 	Name	varchar(max)							,
 
-	primary key	(ID)
+	primary key	(TypeID)
+);
+
+create table [Event] (
+	EventID		int				identity(1,1)	not null,	-- auto -increment feature
+	UserName	varchar(max)					not null,
+	TypeID		int								not null,
+	RegDate		date									,
+	ChangeDate	date									,
+
+	primary key (EventID)
 );
