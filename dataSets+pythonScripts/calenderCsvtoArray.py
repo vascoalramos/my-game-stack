35c07cc48c211d8b7bc361f3179181583ced9b21
Vasco Ramos <vascoalramos@outlook.pt>
2019-05-14 10:32:23 +0100

    Added Reviews dataset

---
+++ dataSets+pythonScripts/calenderCsvtoArray.py
@@ -0,0 +1,20 @@
+import csv
+
+def csv_to_array(baseFile,arrayFile,index,delimiter):
+
+    text_file = open(arrayFile, "w")
+    openFile = open(baseFile, 'r')
+    csvFile = csv.reader(openFile, delimiter=delimiter)
+    firstLine=True
+    line = '['
+    for row in csvFile:
+        line += "'" + row[index] + "',"
+    line += ']'
+    text_file.write(line)
+
+
+    openFile.close()
+    text_file.close()
+
+# csv_to_array("calender.csv", "array.txt",1,',')
+csv_to_array("DataSets/Users.csv","array.txt",0,';')
