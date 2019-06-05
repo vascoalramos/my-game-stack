8300c5d81beb8a944ee2b225f4cb908437fde2d6
Vasco Ramos <vascoalramos@outlook.pt>
2019-06-05 22:24:12 +0100

    Some minor changes

--- Source_code/BD/UDFs/Check Game In List.sql
+++ Source_code/BD/UDFs/Check Game In List.sql
@@ -8,7 +8,11 @@ as
 		if (not exists( select *
 						from GamesDB.Games join GamesDB.GameEventList on GamesDB.Games.GameID=GamesDB.GameEventList.GameID
 							join GamesDB.[Events] on GamesDB.GameEventList.EventID=GamesDB.[Events].EventID
-						where GamesDB.[Events].UserName=@userName and GamesDB.Games.GameID=@gameID and GamesDB.[Events].TypeID = (select TypeID from GamesDB.EventType where Name=@listName and ChangeDate is null)
+						where GamesDB.[Events].UserName=@userName and GamesDB.Games.GameID=@gameID
+							and GamesDB.[Events].TypeID = (select TypeID
+															from GamesDB.EventType
+															where Name=@listName and ChangeDate is null
+															)
 					)
 			)
 			return 0
